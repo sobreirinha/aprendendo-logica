010b76dc07e45c9a4cfd56ce8a80d1901af70df9
Gustavo Sobreira <137525185+sobreirinha@users.noreply.github.com>
2023-08-09 13:55:44 -0300

    create ex01.js

---
+++ ex01/ex01.js
@@ -0,0 +1,8 @@
+
+let num1 = 2;
+let num2 = 4;
+
+console.log('soma: 2 + 4 = ' + (num1 + num2));
+console.log('subtração: 2 - 4 = ' + (num1 - num2));
+console.log('divisão: 2 / 4 = ' + (num1 / num2));
+console.log('multiplicação 2 * 4 = ' + (num1 * num2));
