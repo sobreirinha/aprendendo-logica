3020b5de566feed1a869fed13aca693ca2a0633d
Gustavo Sobreira <137525185+sobreirinha@users.noreply.github.com>
2023-08-10 14:39:27 -0300

    create ex02.js

---
+++ ex01/ex02.js
@@ -0,0 +1,11 @@
+let jantar = 80;
+let garcon = 10; //8
+//let total = jantar + garcon;
+let total = 0;
+
+total = 80 + ((garcon /100) * 80);
+
+
+console.log('Total a pagar: R$' + total);
+console.log(`Total a pagar: R$${jantar},00`);
+console.log(`Taxa do garçin: R$${(garcon/100) * 80},00`);
