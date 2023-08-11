3112f9c14dfd97d6122fb9252012c2c025c6e415
Gustavo Sobreira <137525185+sobreirinha@users.noreply.github.com>
2023-08-11 13:33:30 -0300

    Create ex04 ex05

---
+++ ex01/ex04.js
@@ -0,0 +1,7 @@
+
+function solveMeFirst(a, b) {
+  // Hint: Type return a+b below 
+   return a + b;
+}
+
+console.log(solveMeFirst(7, 3));
---
+++ ex01/ex05.js
@@ -0,0 +1,10 @@
+function simpleArraySum(ar) {
+   // Write your code here
+   let soma=0;
+   for(let index = 0; index < ar.length; index += 1){
+      soma = soma + ar[index]
+   }
+   return soma;
+};
+
+console.log(simpleArraySum([1,2,3,10]));
