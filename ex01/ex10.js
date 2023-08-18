7459776bee25a922836d7ddc7de4b4c0d7a13a4d
Gustavo Sobreira <137525185+sobreirinha@users.noreply.github.com>
2023-08-18 13:28:04 -0300

    ex 10

--- ex01/ex10.js
+++ ex01/ex10.js
@@ -1,3 +1,15 @@
-function plusMinus(arr) {
-   
+function catAndMouse(x, y, z) {
+   let gatoA = x;
+   let gatoB = y;
+   let rato = z;
+   if((Math.abs(rato - gatoA)) < (Math.abs(rato - gatoB))){
+      return 'Cat A';
+   } else if((Math.abs(rato - gatoA)) === (Math.abs(rato - gatoB))){
+      return 'Mouse C';
+   } else {
+      return 'Cat B';
+   }
 }
+
+console.log(catAndMouse(1, 2, 3));
+console.log(catAndMouse(3, 3, 1));
